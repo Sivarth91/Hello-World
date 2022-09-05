8033f467b1b0e25532f88d51293e9aa8b0713def
Sivault-Arthur <81822795+Sivarth91@users.noreply.github.com>
2022-09-05 11:49:21 +0200

    Initial commit

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Hello {
+    static void Main(string[] args) {
+        Console.WriteLine("Hello, World !");
+        Console.ReadLine();
+    }
+}
